c0660a9d34a3169f5fccb4f39746b2fda662488c
장정우 <jungwoo@lotte.net>
2025-09-02 10:31:15 +0900

    20250902 - Spring Reactive Web Applications: Reactor 1부 - Context

---
+++ src/main/java/com/example/reactiveweb/section08/class02/Book.java
@@ -0,0 +1,13 @@
+package com.example.reactiveweb.section08.class02;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@AllArgsConstructor
+@Data
+public class Book {
+    private String isbn;
+    private String bookName;
+    private String author;
+}
