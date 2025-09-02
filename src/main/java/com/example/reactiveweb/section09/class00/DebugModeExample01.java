13a129ce37820fc483ddee3638442b7f6d6cbbbb
장정우 <jungwoo@lotte.net>
2025-09-02 11:05:10 +0900

    20250902 - Spring Reactive Web Applications: Reactor 1부 - debugging

---
+++ src/main/java/com/example/reactiveweb/section09/class00/DebugModeExample01.java
@@ -0,0 +1,15 @@
+package com.example.reactiveweb.section09.class00;
+
+import com.example.reactiveweb.utils.Logger;
+import reactor.core.publisher.Flux;
+
+/**
+ * Non-Debug mode
+ */
+public class DebugModeExample01 {
+    public static void main(String[] args) {
+        Flux.just(2, 4, 6, 8)
+                .zipWith(Flux.just(1, 2, 3, 0), (x, y) -> x/y)
+                .subscribe(Logger::onNext, Logger::onError);
+    }
+}
