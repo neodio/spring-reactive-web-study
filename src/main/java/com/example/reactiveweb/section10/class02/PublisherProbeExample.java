ee887f43811a5967ea95616bfd4871e78b4fde6f
장정우 <jungwoo@lotte.net>
2025-09-03 11:19:11 +0900

    20250903 - Spring Reactive Web Applications: Reactor 1부 - testing

---
+++ src/main/java/com/example/reactiveweb/section10/class02/PublisherProbeExample.java
@@ -0,0 +1,19 @@
+package com.example.reactiveweb.section10.class02;
+
+import reactor.core.publisher.Mono;
+
+public class PublisherProbeExample {
+    public static Mono<String> processWith(Mono<String> main, Mono<String> standby) {
+        return main
+                .flatMap(massage -> Mono.just(massage))
+                .switchIfEmpty(standby);
+    }
+
+    public static Mono<String> useMainPower() {
+        return Mono.empty();
+    }
+
+    public static Mono useStandbyPower() {
+        return Mono.just("# use Standby Power");
+    }
+}
